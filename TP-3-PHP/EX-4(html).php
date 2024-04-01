780bf7e705fe695abcc0c438a6bbb01c16276622
sgmarwa <154100981+sgmarwa@users.noreply.github.com>
2024-04-01 02:59:35 +0000

    Create EX-4(html).php

---
+++ TP-3-PHP/EX-4(html).php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>Résolution d'une équation du deuxième degré</h1>
+    <form action="EX-4(résolution).php" method="post">
+        <label >Coefficient a :</label>
+        <input type="number"  name="a" required>
+        <br>
+        <label >Coefficient b :</label>
+        <input type="number"  name="b" required>
+        <br>
+        <label >Coefficient c :</label>
+        <input type="number"  name="c" required>
+        <br>
+        <input type="submit" value="Résoudre">
+    </form>
+</body>
+</html>
